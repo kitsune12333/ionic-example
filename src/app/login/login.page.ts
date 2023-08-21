66c41a52ce892306f2bb174afa7e9823227d4434
Vina del Mar <vina@duoc.acad>
2023-08-21 13:46:56 -0400

    tercercommit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,17 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { userLogin } from '../model/userLogin';
+import { userLogin } from '../models/userLogin';
+import { Router } from '@angular/router';
+Router
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  import
 })
 export class LoginPage implements OnInit {
+
+  username!: string;
+  password!: string;
+
+  listUser: userLogin[] = [
+    new userLogin(1, 'benja','tortuga', 'ben@ja.com', '55555', 'usuario'),
+    new userLogin(1, 'ajneb','marmota', 'jaja@ja.com', '11111', 'admin'),
+  ]
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+    console.log(this.username, this.password)
+    for(let i = 0; i < this.listUser.length; i++){
+        if(this.username == this.listUser[i].correo && this.password == this.listUser[i].password){
+          if(this.listUser[i].tipo_usuario == 'admin'){
+            //navegar admin
+          }else{
+            //navegar
+          }
+        }else{
+          //user not exit
+        }
+    }
   }
 
 }
--- src/app/usuario/usuario.page.ts
+++ src/app/usuario/usuario.page.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.page.html',
   styleUrls: ['./usuario.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class UsuarioPage implements OnInit {
 
-  constructor() { }
+  constructor(private r:Router) {
+    console.log(this.r.getCurrentNavigation()?.extras.state)
+   }
 
   ngOnInit() {
   }
